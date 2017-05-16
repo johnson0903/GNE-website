cfcb08b8b78a5cfd2d3a255b64e4cd703a104e15
Johnson Lee <johnson50701@gmail.com>
2017-05-16 18:57:27 +0800

    nav 修改 手機版nav-contact 隱藏

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -12,7 +12,7 @@ $(function() {
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 50
+            scrollTop: $($anchor.attr('href')).offset().top - 60
         }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
